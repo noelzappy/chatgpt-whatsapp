cdcdfaf6effbb7f1c526abd2b522d3c417b82dcf
Emmanuel Yeboah <noelzappy@gmail.com>
2023-01-05 22:48:30 +0000

    added local dictionary for personal questions

--- src/services/handleMessage.ts
+++ src/services/handleMessage.ts
@@ -1,12 +1,42 @@
 
+const stringSimilarity = require("string-similarity");
 import { APP_NAME } from ".."
 import { api } from "../configs/chatAPI"
+import data from "../../data.json"
 
 export const handler = async (message: any, prompt: any) => {
     try {
         const start = Date.now()
             
         console.log(`[${APP_NAME}] Received prompt from ` + message.from + ": " + prompt)
+
+        const wordMatch = {
+            index: -1,
+            value: 0
+        }
+
+        data.forEach((res: any, index: number) => { 
+            const {question} = res
+            const similarity = stringSimilarity.compareTwoStrings(question, prompt)
+
+            if (similarity > wordMatch.value) {
+                wordMatch.index = index
+                wordMatch.value = similarity
+            }
+        })
+
+        if (wordMatch.value > 0.5) { 
+            const dataMessage = data[wordMatch.index]
+            
+            const { answers } = dataMessage
+            const randomAnswer = answers[Math.floor(Math.random() * answers.length)]
+
+            console.log(`[${APP_NAME}] Answer to ${message.from}: ${randomAnswer}`)
+
+            message.reply(randomAnswer)
+            return
+        }
+
         const response = await api.sendMessage(prompt)
 
         console.log(`[${APP_NAME}] Answer to ${message.from}: ${response.response}`)
