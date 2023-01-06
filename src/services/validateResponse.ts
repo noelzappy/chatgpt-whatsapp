72bb090d634bdc1f667c4b692e6aa1c2c2526c82
Emmanuel Yeboah <noelzappy@gmail.com>
2023-01-06 00:18:23 +0000

    minor updates to response validation

--- src/services/validateResponse.ts
+++ src/services/validateResponse.ts
@@ -1,5 +1,6 @@
+const stringSimilarity = require("string-similarity");
 
-const wordsDictionary = ["I'm just a computer program", "I am an AI language model", "an artificial intelligence"]
+const wordsDictionary = ["I'm just a computer program", "I am an AI language model", "an artificial intelligence", "I'm just a computer program"]
     
 
 export const validateResponse = (response: String) => {
@@ -8,8 +9,10 @@ export const validateResponse = (response: String) => {
     
     if (wrongStarting) return false
 
-    const words = response.split(" ")
-    const wordsFound = words.filter(word => wordsDictionary.includes(word))
+    // check if the response contains any of the words that are have similarity above 0.7 with the words in the dictionary
+    // This is not an efficient way to do it, but as proof of concept it works
+    const wordsFound = wordsDictionary.filter(word => stringSimilarity.compareTwoStrings(word, response) > 0.7)
+
 
     if (wordsFound.length > 0) return false
 
